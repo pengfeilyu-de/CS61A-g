a32a41075e9bd4c9764a32c35c5fc77478440bdf
pengfeilyu-de <56892479+pengfeilyu-de@users.noreply.github.com>
2022-11-30 11:12:39 +0100

    Add files via upload

---
+++ Labs/lab01.py
@@ -0,0 +1,81 @@
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    >>> falling(4, 0)
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    total = 1
+    stop = n - k
+    while n > stop:
+        total = total * n
+        n = n - 1
+    return total
+
+
+def sum_digits(y):
+    """Sum all the digits of y.
+
+    >>> sum_digits(10) # 1 + 0 = 1
+    1
+    >>> sum_digits(4224) # 4 + 2 + 2 + 4 = 12
+    12
+    >>> sum_digits(1234567890)
+    45
+    >>> a = sum_digits(123) # make sure that you are using return rather than print
+    >>> a
+    6
+    """
+    "*** YOUR CODE HERE ***"
+    total = 0
+    while y > 0:
+        total = total + y % 10
+        y = y // 10
+    return total
+
+
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    total = 0
+    l = []
+    n = abs(n)
+    m = n // 10
+    p = m
+    o = 0
+    while ((n % 2 == 0) & (n > 0)):
+        l.append(n % 100)
+        n = n // 100
+        l.append(p % 100)
+        p = p // 100
+    while ((n % 2 == 1) & (n > 0)):
+        l.append(n % 100)
+        n = n // 100
+        l.append(p % 100)
+        p = p // 100
+
+    """print(l)"""
+
+    for x in l[::-1]:
+        if x == 88:
+            o = o + 1
+    return o > 0
